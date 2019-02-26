369d6a5e2eb287e92d1a2f1ce09819244e52ab8a
ArijChammakhi <47174749+ArijChammakhi@users.noreply.github.com>
2019-02-26 19:56:29 +0100

    Add files via upload

--- PIDEV/src/PIDEVBundle/Repository/WorkshopRepository.php
+++ PIDEV/src/PIDEVBundle/Repository/WorkshopRepository.php
@@ -10,4 +10,55 @@ namespace PIDEVBundle\Repository;
  */
 class WorkshopRepository extends \Doctrine\ORM\EntityRepository
 {
+
+    public function SelectWorkshopByidUSer($iduser)
+{
+    $etatworkshop="en cours";
+
+    $query = $this->createQueryBuilder('f')
+        // ->Where('f.idUser=:iduser')
+        ->Where('f.etatWork=:etatworkshop')
+        ->andWhere('f.idUser=:iduser')
+
+        ->setParameter(':etatworkshop',$etatworkshop)
+        ->setParameter(':iduser',$iduser);
+    return $query->getQuery()->getResult();
+}
+    public function SelectWorkshopByidSession($idSession)
+    {
+        $etatworkshop="en cours";
+        $query = $this->createQueryBuilder('s')
+            ->Where('s.etatWork = :etatWork')
+            ->andWhere('s.idSession=:idSession')
+            ->setParameter(':idSession',$idSession)
+            ->setParameter(':etatWork',$etatworkshop);
+        return $query->getQuery()->getResult();
+    }
+    public function findByDateDql($date){
+        $etatworkshop="en cours";
+        $query = $this->createQueryBuilder('s')
+            ->Where('s.etatWork = :etatWork')
+            ->andWhere('s.date>:date')
+            ->setParameter(':date',$date)
+            ->setParameter(':etatWork',$etatworkshop);
+        return $query->getQuery()->getResult();
+    }
+    public function findByDatearchive($date){
+        $etatworkshop="en cours";
+        $query = $this->createQueryBuilder('s')
+            ->Where('s.etatWork = :etatWork')
+            ->andWhere('s.date<:date')
+            ->setParameter(':date',$date)
+            ->setParameter(':etatWork',$etatworkshop);
+        return $query->getQuery()->getResult();
+    }
+    public function findAnnonce2($search)
+    {
+        return $this->createQueryBuilder('e')
+            ->where('UPPER(e.nom) LIKE UPPER(:search)')
+            ->setParameter('search', '%'.$search.'%')
+            ->getQuery()
+            ->getResult();
+
+    }
 }
